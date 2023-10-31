da862b8926d6ef59739b0cbcc7032df4b5f04d0e
LOSTCOLE <colebarber977@gmail.com>
2023-10-30 19:39:20 -0500

    skeleton UI-7:39

--- my-app-stripe/src/routes/+page.svelte
+++ my-app-stripe/src/routes/+page.svelte
@@ -1,20 +1,8 @@
-<script>
-	const { sveltekit } = require("@sveltejs/kit/vite");
 
-</script>
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<div class= "grid grid-cols-3 gap-4">
-			<div class="col-span-3">
-				<h1>sveltekit 1.0 Store</h1>
-		</div>	
-	</div>
-		<div class= "col-span-3">
-			<button class="btn variant-filled-primary"> checkout with Stripe API</button>>
-				
-		<h2 class="h2">Let's get cracking bones!</h2>
+		<h1 class="h1">Let's get cracking bones!</h1>
 		<p>Start by exploring:</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
@@ -25,4 +13,3 @@
 		</ul>
 	</div>
 </div>
-
