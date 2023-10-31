82f2078e5be41e98e2565afba2abad7afc6ee849
LOSTCOLE <colebarber977@gmail.com>
2023-10-30 19:46:43 -0500

    skeleton UI-addbutton-7:45

--- my-app-stripe/src/routes/+page.svelte
+++ my-app-stripe/src/routes/+page.svelte
@@ -1,10 +1,18 @@
+<script>
+	const { sveltekit } = require("@sveltejs/kit/vite");
+
+</script>
 
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
+	<div class="grit grid-cols-3 gap-4">
+		<div class ="col-spam-3">
+			<h1>sveltekit 1.0 store</h1>
+		</div>   
+		<div class ="col-spam-3">
+			<button class= "btn variant-filled-primary"> Checkout with Stripe API	</button>>
+		</div>
+			<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
 			<li><code class="code">/src/app.postcss</code> - app wide css</li>
 			<li>
