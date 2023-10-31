8d6ff1ca149a0b02965bb22ca7e7fb5a0ef9f4da
LOSTCOLE <colebarber977@gmail.com>
2023-10-30 20:02:31 -0500

    skeleton UI-addbutton-7:45

--- my-app-stripe/src/routes/+layout.svelte
+++ my-app-stripe/src/routes/+layout.svelte
@@ -1,10 +1,6 @@
-<script lang="ts">
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
-
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
-
 <slot />
--- my-app-stripe/src/routes/+page.svelte
+++ my-app-stripe/src/routes/+page.svelte
@@ -1,23 +1,54 @@
-<script>
-	const { sveltekit } = require("@sveltejs/kit/vite");
+<script lang="ts">
+	import ProductCard from "$lib/productCard.svelte";
+	import { get } from "svelte/store";
+	import { cartItems } from "../cart";
+
+	const products : Product[] = [
+		{
+			id: "price_1McfalGEQ8u28rQ5BZ8bAlqT",
+			name: "Coffee",
+			price: 5
+		},
+		{
+			id: "price_1McfbuGEQ8u28rQ5mEO9kZeB",
+			name: "Sunglasses",
+			price: 10
+		},
+		{
+			id: "price_1McfcaGEQ8u28rQ56pMnyDz1",
+			name: "Water Bottle",
+			price: 15
+		}
+	]
+
+	async function checkout() {
+		await fetch("api/stripeCheckout", { // http://localhost:5173/api/stripeCheckout
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(
+				{ items: get(cartItems) }
+			)
+		}).then((data) => {
+			return data.json()
+		}).then((data) => {
+			window.location.replace(data.url);
+		});
+	}
 
 </script>
 
-
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="grit grid-cols-3 gap-4">
-		<div class ="col-spam-3">
-			<h1>sveltekit 1.0 store</h1>
-		</div>   
-		<div class ="col-spam-3">
-			<button class= "btn variant-filled-primary"> Checkout with Stripe API	</button>>
+	<div class="grid grid-cols-3 gap-4">
+		<div class="col-span-3">
+			<h1>SvelteKit 1.0 Store</h1>
 		</div>
-			<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+		{#each products as product}
+			<ProductCard product={product}/>
+		{/each}
+		<div class="col-span-3">
+			<button class="btn variant-filled-primary" on:click={() => checkout()}>Checkout with Stripe API</button>
+		</div>
 	</div>
 </div>
